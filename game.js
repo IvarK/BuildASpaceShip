2593c1c844d462282528af2262b6d6c5a2402f3d
IvarK <IvarK@users.noreply.github.com>
2016-09-09 01:05:57 +0300

    Update game.js

--- game.js
+++ game.js
@@ -368,11 +368,11 @@ setInterval(function() {
   lastUpdate = thisUpdate;
 }, 100);
 shipUpdateButton.innerHTML = shortenCosts(player.shipUpdateCost) + " € to update your ship";
-document.getElementById("shipUpdateAmount").innerHTML = "0/5";
+document.getElementById("shipUpdateAmount").innerHTML = player.shipUpdateAmount + "/5";
 rocketUpdateButton.innerHTML = shortenCosts(player.rocketUpdateCost) + " € to update your rockets";
-document.getElementById("rocketUpdateAmount").innerHTML = "0/5";
+document.getElementById("rocketUpdateAmount").innerHTML = player.rocketUpdateAmount + "/5";
 wingUpdateButton.innerHTML = shortenCosts(player.wingUpdateCost) + " € to update your wings";
-document.getElementById("wingUpdateAmount").innerHTML = "0/5";
+document.getElementById("wingUpdateAmount").innerHTML = player.wingUpdateAmount + "/5";
 rocketButton.innerHTML = shortenCosts(player.rocketCost) + " € for " + Rockets[player.rockets] + " rocket.";
 wingButton.innerHTML = shortenCosts(player.wingCost) + " € for " + Wings[player.wings] + " wings.";
 shipButton.innerHTML = shortenCosts(player.shipCost) + " € for " + Ships[player.ships] + ".";
