d7df252690c93333ad07703f91431441aaa91845
IvarK <IvarK@users.noreply.github.com>
2016-09-30 01:21:41 +0300

    Update game.js

--- game.js
+++ game.js
@@ -730,7 +730,7 @@ setInterval(function() {
     document.getElementById("totalMoney").innerHTML = "You have made a total of " + shorten(player.totalMoney) + " €"
   document.getElementById("totalDistance").innerHTML = "You have travelled a total of " + distanceIndicators(player.totalDistance)
   document.getElementById("nextPlanet").innerHTML = "Distance to the next exoplanet: " + distanceIndicators(Math.max((player.nextPlanet - player.distance), 0))
-  
+  document.getElementById("refugeesOnPrestige").innerHTML = "You get " + shortenCosts(getPrestige()-player.prestigeAmount) + " refugees on reset"
 
 
   
